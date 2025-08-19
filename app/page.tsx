3cadff66633ee1e3dc99f9a670f19a2bba782e65
Xander Wayne <xandernzaywa@gmail.com>
2025-08-19 10:53:33 +0300

    Sizing of the home cards

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
 
   return (
     <>
-      <div className="flex flex-1 flex-col gap-4 p-4">
+      <div className="flex flex-1 flex-col gap-4">
         <DayTotal />
         <AddEntryForm />
         <EntriesTable />
