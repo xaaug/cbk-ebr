1adeebefa1ef035a32f4fcdadd4171943ca7e9df
Xander Wayne <xandernzaywa@gmail.com>
2025-08-23 18:30:21 +0300

    Chane the font sizes

--- components/GreetingsCard.tsx
+++ components/GreetingsCard.tsx
@@ -5,14 +5,14 @@ export default function GreetingsCard() {
 
   const hour = new Date().getHours();
   const greeting =
-    hour < 12 ? "Good Morning" : hour < 18 ? "Good Afternoon" : "Good Evening";
+    hour < 12 ? "Good Morning" : hour < 16 ? "Good Afternoon" : "Good Evening";
 
   return(
     <>
       <div className="w-full h-[10rem] rounded-xl shadow-md bg-primary-foreground dark:bg-neutral-900 flex items-center justify-between overflow-hidden">
       {/* Left Section */}
       <div className="flex flex-col justify-center pl-6 text-white">
-        <h2 className="text-xl font-semibold font-serif">{greeting},</h2>
+        <h2 className="text-lg font-semibold font-serif">{greeting},</h2>
         <p className="text-lg font-serif">Francis</p>
       </div>
 
